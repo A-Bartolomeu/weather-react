52e0a60adcc18d4152cfa893f2cadde7c46bdddc
ABartolomeu-coder <124580703+ABartolomeu-coder@users.noreply.github.com>
2023-08-17 16:31:52 +0100

    Added alt in img

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -22,5 +22,5 @@ export default function WeatherIcon(props) {
     "50n": "./img/clear-night.svg",
   };
 
-  return <img src={codeMapping[props.code]} />;
+  return <img src={codeMapping[props.code]} alt="image" />;
 }
